c2c6ee20f83dd5f194d1e009285975956b1ba5eb
michaelshxw <michaelshxw@gmail.com>
2021-06-15 17:17:43 +0800

    add routes for testing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,15 +7,28 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/login', async (req, res) => {
-  res.render('login')
+  res.render('success')
 });
 
 router.get('/signup', async (req, res) => {
-  res.render('signup')
+  res.render('success')
+});
+
+router.get('/setupprofile1', async (req, res) => {
+  res.render('setupprofile1')
 });
 
 router.get('/plan', async (req, res) => {
   res.render('mealplan')
+});
+
+
+router.get('/setupprofile2', async (req, res) => {
+  res.render('setupprofile2')
+});
+
+router.get('/setupprofile3', async (req, res) => {
+  res.render('setupprofile3')
 });
 
 
