5182670f2defb09ab34404b9766f9bd1fd440171
michaelshxw <michaelshxw@gmail.com>
2021-06-15 17:18:05 +0800

    add signup to login.js

--- public/js/login.js
+++ public/js/login.js
@@ -19,4 +19,28 @@ const loginFormHandler = async (event) => {
     }
   };
 
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  document.querySelector('.loginBtn').addEventListener('submit', loginFormHandler);
+
+  const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users/', {
+        method: 'POST',
+        body: JSON.stringify({ username, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/plan');
+      } else {
+        alert('Failed to sign up.');
+      }
+    }
+  };
+  
+  document.querySelector('.signUpBtn').addEventListener('submit', signupFormHandler);
