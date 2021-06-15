f682a07867a2c0ed1dfa9cb0330383c54ed46c40
michaelshxw <michaelshxw@gmail.com>
2021-06-15 16:23:32 +0800

    fix bcrypt error

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const sequelize = require('../config/connection');
 
 class User extends Model {
   checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
+    return  bcrypt.compareSync(loginPw, this.password);
   }
 }
 
