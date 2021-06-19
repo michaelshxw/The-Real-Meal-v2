3de928e1e8ae9f91eceaf0bfde248009c40e5b52
michaelshxw <michaelshxw@gmail.com>
2021-06-19 16:02:01 +0800

    update homeRoutes for login/signup

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,35 +3,48 @@ const { User } = require('../models');
 
 // GET all posts for homepage
 router.get('/', async (req, res) => {
-  res.render('landingpage')
+  if (req.session.loggedIn) {
+    res.redirect('/home');
+    return;
+  } else {
+    res.render('login');
+  }
 });
 
 router.get('/home', async (req, res) => {
-  res.render('home')
+  res.render('home', {
+    loggedIn: req.session.loggedIn
+  }) 
 });
 
 router.get('/setup1', async (req, res) => {
-  res.render('setupprofile1')
+  res.render('profile')
 });
 
 router.get('/setup2', async (req, res) => {
-  res.render('setupprofile2') 
+  res.render('setupprofile2')
 });
 
 router.get('/setup3', async (req, res) => {
   res.render('setupprofile3')
 });
 
-router.get('/dailyplan', async (req, res) => {
-  res.render('dailyplan')
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  } else {
+    res.render('login');
+  }
+
 });
 
 
 
 
 
-  
-  
+
+
 
 
 module.exports = router;
