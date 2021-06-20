4bd909819898bd22432c726148d7048359808d3d
michaelshxw <michaelshxw@gmail.com>
2021-06-20 21:52:40 +0800

    add userProfile to main manually

--- models/userProfile.js
+++ models/userProfile.js
@@ -1,1 +1,91 @@
 // create model for user profile details
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class UserProfile extends Model { User }
+
+UserProfile.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    dob: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight: 
+    {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    height: 
+    {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weightGoal: 
+    {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    activityLevel: 
+    {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dietaryPreference: 
+    {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    allergies: 
+    {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    excludeFoods: 
+    {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    calorieLimit: 
+    {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    measurementSystem: 
+    {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    carbs: 
+    {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    fat: 
+    {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    protein: 
+    {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'UserProfile',
+  }
+);
+
+module.exports = UserProfile;
