baf0d6dfb962f2d500ee1e435efbd6a2549e2eb9
michaelshxw <michaelshxw@gmail.com>
2021-06-21 01:31:48 +0800

    switching branches

--- public/js/dailyplan.js
+++ public/js/dailyplan.js
@@ -1,5 +1,5 @@
 // import required modules
-// const userProfile = require("../../models/UserProfile")
+const userProfile = require("../../models/UserProfile")
 
 // declare variables 
 let generatePlan = "https://api.spoonacular.com/mealplanner/generate";
@@ -19,17 +19,45 @@ let userExclude = "butter";
 const dailyURL = generatePlan + apiKey + dailyPlan + targetCalories + userCalories + dietPref + userDietPref + exclude + userExclude;
 console.log(dailyURL);
 
-// http request with the dailyURL
-$.ajax({
-    url: dailyURL,
-    type: 'GET',
-    success: function(res) {
-        console.log(res);
-    },
-    dataType: "json",
-    error: function() {
-        alert("Couldn't retrieve daily meal plan. Please try again.");
-    }
-});
 
+// http request with the dailyURL
+function dailyHttpRequest() {
+    $.ajax({
+        url: dailyURL,
+        type: 'GET',
+        success: function (res) {
+            let cardBody = document.getElementsByClassName('card-body')
+            let cardBodyChildren;
+            console.log(res.meals);
+            for (let i = 0; i < cardBody.length; i++) {
+                cardBodyChildren = cardBody[i].children;
+                cardBodyChildren[0].textContent = res.meals[i].title;
+            }
+
+
+
+
+            // recipe title one
+            // let recipeTitleOne = document.querySelector('.recipeTitleOne');
+            // let titleOne = res.meals[0].title;
+            // recipeTitleOne.append(titleOne)
+            // // recipe title two
+            // let recipeTitleTwo = document.querySelector('.recipeTitleTwo');
+            // let titleTwo = res.meals[1].title;
+            // recipeTitleTwo.append(titleTwo)
+            // // recipe title three
+            // let recipeTitleThree = document.querySelector('.recipeTitleThree');
+            // let titleThree = res.meals[2].title;
+            // recipeTitleThree.append(titleThree)
+        },
+        dataType: "json",
+        error: function () {
+            alert("Couldn't retrieve daily meal plan. Please try again.");
+        },
+    });
+}
+dailyHttpRequest();
+// function dataHandler(data) {
+//     console.log(JSON.parse(data));
+// }
 // To get the id, summary and title of the recipe, use: https://api.spoonacular.com/recipes/{{id}}/summary
