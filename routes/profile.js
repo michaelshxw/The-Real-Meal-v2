c49d56e3758fc523638f0c3d6080a16ff78470e3
BRASS <mr.brn.lewis@outlook.com>
2021-06-20 13:00:44 +0800

    profile router change

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,28 @@
 //modulised router 
 const router = require('express').Router();
-const users = require("../controllers/users");
+const profile = require("../controllers/profile");
 
-router.get('/',(req,res) => {res.render("../views/setup-profile-one")}); // same as /profile
+router.get('/',async (req,res) =>
+{
+    console.log(req.session);
 
-router.post('/',(req,res) => {console.log("post request profile")}); // same as /profile
+    try
+    {
+    if(req.session.user == undefined)
+        {
+        res.redirect("/");
+        }
+        else
+        {
+        res.render("../views/setup-profile-one"); // same as /profile
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
+    
+});
+
+
 
 module.exports = router;
