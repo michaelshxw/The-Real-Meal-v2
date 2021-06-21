e16ad6cfb4f2f3cbb3b3b100b6d9ec956b44aa85
BRASS <mr.brn.lewis@outlook.com>
2021-06-21 16:24:00 +0800

    heroku updates

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
 
-var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,
-  {
-   dialect:"mysql", 
-   host:"localhost",
-   port:"3306"
-  });
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
